78b6be1b47b8fb86dc0e6202b86b146414c54d87
arbouch <mohamedarbi458@gmail.com>
2020-09-02 14:44:46 +0200

    Done!

---
+++ ChatSignalR/ChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalR.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public async Task sendMessage(string fromUser, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", fromUser, message);
+        }
+    }
+}
---
+++ ChatSignalR/ChatSignalR/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var msg = fromUser + ": " + message;
+    var li = document.createElement("li");
+    li.textContent = msg
+    $("#list").prepend(li);
+});
+connection.start();
+$("#btnSend").on("click", function () {
+    var fromUser = $("#txtUser").val();
+    var message = $("#TxtMessage").val();
+    connection.invoke("sendMessage", fromUser, message);
+});
